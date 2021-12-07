5e77f51521bafa0d5fb3a7bb5fe9e849712d63e2
blackramble <bjtfetopa@gmail.com>
2021-12-07 20:09:39 +0800

    [更新]bank

--- week1-211207/Bank-JS/solution-1.js
+++ week1-211207/Bank-JS/solution-1.js
@@ -6,3 +6,19 @@
  * 題名：Remove Duplicates from Sorted Array
  * 連結：https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
  */
+
+var removeDuplicates = function (nums) {
+  let front = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[front] !== nums[i]) {
+      front += 1;
+      nums[front] = nums[i];
+    }
+  }
+
+  console.log(nums);
+  return front + 1;
+};
+
+console.log(removeDuplicates([1, 1, 3, 3, 3, 5, 8]));
