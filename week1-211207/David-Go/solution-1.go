2b491ee253ea2953f418951df6d144c9fd68e419
David <yasta55688@gmail.com>
2021-12-07 19:03:34 +0800

    [更新] 解答完成

--- week1-211207/David-Go/solution-1.go
+++ week1-211207/David-Go/solution-1.go
@@ -6,3 +6,20 @@
  * 題名：Remove Duplicates from Sorted Array
  * 連結：https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
  */
+
+ collect := []int{} 
+ for _ , v:= range nums{
+ 
+	inCollect := false
+	for i := 0 ; i < len(collect) ;i++{
+	  inCollect = collect[i] == v 
+	}
+	
+	if !inCollect {
+		collect = append(collect,v)
+	}
+
+}	
+ 
+return len(collect)
+ 
