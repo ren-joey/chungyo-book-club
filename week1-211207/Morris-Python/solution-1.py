e5f285255a23daddb6bdd4cf7adef9d155e9cf65
ren-joey <tsuyiren@gmail.com>
2021-12-07 17:22:16 +0800

    [DEV] initialization

---
+++ week1-211207/Morris-Python/solution-1.py
@@ -0,0 +1,8 @@
+'''
+===== 技術集會 WEEK1 =====
+本周題目
+主題：Top-Interview-questions-easy
+類別：Array
+題名：Remove Duplicates from Sorted Array
+連結：https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+'''
