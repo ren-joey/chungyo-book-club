45af2d656f8c7088ca8d2b376e5060866f3a94c4
blackramble <bjtfetopa@gmail.com>
2021-12-14 19:56:21 +0800

    [更新]bank

--- week2-211214/Bank-JS/solution-1.js
+++ week2-211214/Bank-JS/solution-1.js
@@ -6,3 +6,18 @@
  * 題名：First Unique Character in a String
  * 連結：https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/
  */
+
+const s = "ltoccde";
+const obj = [];
+const test = s.split("");
+
+function output() {
+  for (let i = 0; i < test.length; i += 1) {
+    if (test.indexOf(test[i]) === test.lastIndexOf(test[i])) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+output();
