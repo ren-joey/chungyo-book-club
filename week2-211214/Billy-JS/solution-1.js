5bd5b4ed943610d27826db64233e185def659790
chenlinchu <chenlinchu637@gmail.com>
2021-12-14 19:41:28 +0800

    [更新] Billy - 週二繳交

--- week2-211214/Billy-JS/solution-1.js
+++ week2-211214/Billy-JS/solution-1.js
@@ -6,3 +6,12 @@
  * 題名：First Unique Character in a String
  * 連結：https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/
  */
+
+var firstUniqChar = function(s) {
+    for (i=0; i < s.length; i += 1) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+            return i;
+        }
+    }
+    return -1;
+};
