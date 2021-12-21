2cf8be42f50f9857c29395c0371dfc91246efe0d
ms0223900 <ms0223900@gmail.com>
2021-12-21 22:26:19 +0800

    feat: update solutions of merge sorted arrays

--- week3-211221/Penguin-TS/solution-1.ts
+++ week3-211221/Penguin-TS/solution-1.ts
@@ -12,35 +12,68 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
+
+// sol1
+// function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+//     let _nums1 = [...nums1];
+//     let nums1Idx = 0;
+//     let nums2Idx = 0;
+
+//     for (let i = 0; i < nums1.length; i++) {
+//         const currentNum1 = _nums1[nums1Idx];
+//         const currentNum2 = nums2[nums2Idx];
+//         // console.log(nums1, currentNum1, currentNum2, nums1Idx, nums2Idx)
+
+//         if(nums1Idx === (m)) {
+//             nums1[i] = currentNum2;
+//             nums2Idx++;
+//         }
+//         else if(nums2Idx === (n)) {
+//             nums1[i] = currentNum1;
+//             nums1Idx++;
+//         }
+//         else {
+//             if(currentNum1 < currentNum2) {
+//                 nums1[i] = currentNum1;
+//                 nums1Idx++;
+//             } else {
+//                 nums1[i] = currentNum2;
+//                 nums2Idx++;
+//             }
+//         }
+//     }
+//     // console.log(nums1)
+// };
+
+// sol2
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let _nums1 = [...nums1];
-    let nums1Idx = 0;
-    let nums2Idx = 0;
+    for (let i = nums1.length - 1; i >= 0; i--) {
+        if(n && m) {
+            const num1 = nums1[m - 1];
+            const num2 = nums2[n - 1];
 
-    for (let i = 0; i < nums1.length; i++) {
-        const currentNum1 = _nums1[nums1Idx];
-        const currentNum2 = nums2[nums2Idx];
-        console.log(nums1,)
-
-        if(nums1Idx === (m - 1)) {
-            nums1[i] = currentNum2;
-        }
-        else if(nums2Idx === (n - 1)) {
-            nums1[i] = currentNum1;
-        }
-        else {
-            if(currentNum1 > currentNum2) {
-                nums1[i] = currentNum2;
-                nums2Idx++;
+            if(num1 > num2) {
+                nums1[i] = num1;
+                m--;
             } else {
-                nums1[i] = currentNum1;
-                nums1Idx++;
+                nums1[i] = num2;
+                n--;
+            }
+            // break;
+        } else {
+            if(n) {
+                nums1[i] = nums2[n - 1];
+                n--;
+            }
+            if(m) {
+                nums1[i] = nums1[m - 1];
+                m--;
             }
         }
+        console.log(nums1)
     }
-};
+}
 
-// not solved
 
 merge(
     [1,2,3,0,0,0],
