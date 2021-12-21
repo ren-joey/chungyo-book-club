359454bbe93b28aca44cb47e1285f4b93910f2e9
ren-joey <tsuyiren@gmail.com>
2021-12-21 13:08:21 +0800

    [開發] WEEK3 題目

--- week3-211221/Morris-Python/solution-1.py
+++ week3-211221/Morris-Python/solution-1.py
@@ -1,8 +1,10 @@
 '''
-===== 技術集會 WEEK3 =====
-本周題目
-主題：
-類別：
-題名：
-連結：
+/**
+ * ===== 技術集會 WEEK3 =====
+ * 本周題目 [EASY]
+ * 連結：https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/587/
+ *
+ * 本周題目 [MEDIUM]
+ * 連結：https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
+ */
 '''
