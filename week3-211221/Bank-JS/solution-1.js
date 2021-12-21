b1bdeac85cef5dcc25556964b89ff0a9f0f501d9
blackramble <bjtfetopa@gmail.com>
2021-12-21 19:32:59 +0800

    [新增]bank

--- week3-211221/Bank-JS/solution-1.js
+++ week3-211221/Bank-JS/solution-1.js
@@ -6,3 +6,20 @@
  * 本周題目 [MEDIUM]
  * 連結：https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
  */
+
+const nums1 = [1, 2, 3, 0, 0, 0];
+const m = 3;
+const nums2 = [2, 5, 6];
+const n = 3;
+
+const fuc = (nums1, m, nums2, n) => {
+  const newnums1 = nums1.map((item) => {
+    if (item === 0) {
+      return;
+    }
+    return item;
+  });
+  console.log(newnums1);
+};
+
+fuc();
