5b07e75a79acbf8d9ba4bfed157a5b52c7b7d24e
David <yasta55688@gmail.com>
2021-12-21 19:33:46 +0800

    [開發]  上傳作答

--- week3-211221/David-Go/solution-1.go
+++ week3-211221/David-Go/solution-1.go
@@ -6,3 +6,40 @@
  * 本周題目 [MEDIUM]
  * 連結：https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
  */
+
+func merge(nums1 []int, m int, nums2 []int, n int) []int {
+
+	// 先將　nums1 由小到大排序
+	// result := []int{}
+
+	// 合併數組 並由小到大 排序 且 必須大於 0
+	for i := 0; i < (m + n - 1); i++ {
+		for j := 0; j < len(nums2); j++ {
+			// n1 > n2 位置互換
+			if nums1[i] > nums2[j] {
+				b := nums2[j]
+				nums2[j] = nums1[i]
+				nums1[i] = b
+			}
+
+			if nums1[i] > nums2[j] {
+				b := nums2[j]
+				nums2[j] = nums1[i]
+				nums1[i] = b
+			}
+		}
+	}
+
+	fmt.Println(nums2)
+
+	return nums1
+}
+
+/**
+* 有包含球的盒子
+*
+ */
+func minOperations(boxes string) []int {
+
+	return []
+}
