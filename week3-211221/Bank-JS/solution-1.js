ef7cbd3122e45c499896c7e8317dc02bc49aaace
blackramble <bjtfetopa@gmail.com>
2021-12-21 19:34:21 +0800

    [修改]bank忘記存檔

--- week3-211221/Bank-JS/solution-1.js
+++ week3-211221/Bank-JS/solution-1.js
@@ -7,19 +7,19 @@
  * 連結：https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
  */
 
-const nums1 = [1, 2, 3, 0, 0, 0];
-const m = 3;
-const nums2 = [2, 5, 6];
-const n = 3;
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, nums1.length - m);
 
-const fuc = (nums1, m, nums2, n) => {
-  const newnums1 = nums1.map((item) => {
-    if (item === 0) {
-      return;
+  var i = 0;
+  var j = 0;
+
+  while (j < nums2.length) {
+    if (nums1[i] === undefined || nums1[i] > nums2[j]) {
+      nums1.splice(i, 0, nums2[j]);
+      j++;
+      i++;
+    } else {
+      i++;
     }
-    return item;
-  });
-  console.log(newnums1);
+  }
 };
-
-fuc();
