fddadad0c3d54c2289fa8c1b55250f161edef3a3
ren-joey <tsuyiren@gmail.com>
2022-01-04 17:22:00 +0800

    [開發] week 5 quest upload

---
+++ week5-220104/Morris-Python/solution-1.py
@@ -0,0 +1,10 @@
+'''
+/**
+ * ===== 技術集會 WEEK4 =====
+ * 本周題目 [EASY]
+ * 連結：https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
+ *
+ * 本周題目 [MEDIUM]
+ * 連結：https://leetcode.com/problems/partition-labels/
+ */
+'''
