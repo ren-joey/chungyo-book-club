38a7054ba4a7cb2d33da5fb81e9438a6ca2ce9e1
ms0223900 <ms0223900@gmail.com>
2022-01-04 20:49:37 +0800

    feat: update solutions

--- week5-220104/Penguin-TS/solution-1.ts
+++ week5-220104/Penguin-TS/solution-1.ts
@@ -6,3 +6,88 @@
  * 本周題目 [MEDIUM]
  * 連結：https://leetcode.com/problems/partition-labels/
  */
+
+function plusOne(digits: number[]): number[] {
+  const lastIdx = digits.length - 1;
+
+  for (let i = lastIdx; i >= 0; i--) {
+    if(digits[i] < 9) {
+      digits[i] += 1;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  
+  digits = [1, ...digits];
+
+  return digits;
+}
+
+// function plusOne(digits: number[]): number[] {
+//   return (Number(digits.join('')) + 1).toString().split('').map(s => Number(s))
+// }
+
+// console.log(
+//   plusOne([9, 9, 9])
+// )
+
+// function partitionLabels(s: string): number[] {
+//   let charFirstLastIdx: Record<string, number[]> = {};
+
+//   for (let i = 0; i < s.length; i++) {
+//     const char = s.charAt(i);
+//     if(!charFirstLastIdx[char]) {
+//       charFirstLastIdx[char] = [i];
+//     } else {
+//       charFirstLastIdx[char][1] = i;
+//     }
+//   }
+
+//   // console.log(charFirstLastIdx)
+
+//   // let found = {};
+//   let res: number[] = [];
+//   let startIdx = 0;
+//   let lastIdx = 0;
+
+//   for (let j = 0; j < s.length; j++) {
+//     const char = s[j];
+//     lastIdx = Math.max(charFirstLastIdx[char][1] || charFirstLastIdx[char][0], lastIdx);
+//     if(lastIdx === j) {
+//       res.push(lastIdx - startIdx + 1);
+//       startIdx = lastIdx + 1;
+//     }
+//   }
+
+//   return res;
+// };
+
+function partitionLabels(s: string): number[] {
+  let charLastIdx: Record<string, number> = {};
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const char = s.charAt(i);
+    if(!charLastIdx[char]) {
+      charLastIdx[char] = i;
+    }
+  }
+
+  let res: number[] = [];
+  let startIdx = 0;
+  let lastIdx = 0;
+
+  for (let j = 0; j < s.length; j++) {
+    const char = s[j];
+    lastIdx = Math.max(charLastIdx[char], lastIdx);
+    if(lastIdx === j) {
+      res.push(lastIdx - startIdx + 1);
+      startIdx = lastIdx + 1;
+    }
+  }
+
+  return res;
+};
+
+console.log(
+  partitionLabels("ababcbacadefegdehijhklij")
+)
