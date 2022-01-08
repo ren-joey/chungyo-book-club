da169fd6419a12ebee2e956e8022c95e8ea11a09
ChenLin Chu <chenlinchu@ChenLins-Macbook.local>
2022-01-08 14:29:08 +0800

    [更新] Billy 繳交

--- week5-220104/Billy-JS/solution-1.js
+++ week5-220104/Billy-JS/solution-1.js
@@ -6,3 +6,50 @@
  * 本周題目 [MEDIUM]
  * 連結：https://leetcode.com/problems/partition-labels/
  */
+
+// [EASY]
+const plusOne = (arr) => {
+    let isNextPlusOne = false;
+    arr[arr.length - 1] += 1;
+
+    for (let i = arr.length - 1; i >= 0; i -= 1) {
+        if (isNextPlusOne) {
+            arr[i] += 1;
+            isNextPlusOne = false;
+        }
+
+        if (arr[i].toString().length > 1) {
+            isNextPlusOne = true;
+            arr[i] = arr[i] % 10;
+        }
+
+        if (i === 0 && isNextPlusOne) {
+            arr.unshift(1);
+            break;
+        }
+    }
+
+    return arr;
+};
+
+// [MEDIUM]
+const partitionLabels = (str) => {
+    let lastIdx = {};
+
+    for (let i = 0; i < str.length; i += 1) lastIdx[str[i]] = i;
+
+    let res = [];
+    let start = 0;
+    let end = 0;
+
+    for (let i = 0; i < str.length; i += 1) {
+        end = Math.max(end, lastIdx[str[i]]);
+
+        if (i === end) {
+            res.push(end - start + 1);
+            start = end + 1;
+        }
+    }
+
+    return res;
+};
