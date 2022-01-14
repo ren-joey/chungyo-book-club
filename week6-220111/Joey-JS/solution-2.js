126a39e008b103c654ca7dea90b241566a18ea8d
任祖頤 <joey@renzuyis-MacBook-Pro.local>
2022-01-14 15:40:58 +0800

    [開發] week6 joey update

--- week6-220111/Joey-JS/solution-2.js
+++ week6-220111/Joey-JS/solution-2.js
@@ -3,3 +3,22 @@
  * 本周題目 [MEDIUM]
  * 連結：https://leetcode.com/problems/find-the-winner-of-the-circular-game/
  */
+
+ var findTheWinner = function(n, k) {
+    let currentPos = 0;
+    let seats = [];
+
+    for (let i = 0; i < n; i++) {
+        seats.push(i + 1);
+    }
+
+    while(seats.length > 1) {
+        currentPos = (currentPos + k - 1) % seats.length;
+        seats.splice(currentPos, 1);
+        currentPos = currentPos % seats.length;
+    }
+
+    return seats[0];
+};
+
+// findTheWinner(6, 5);
